0476bcfb9134917635763df5f4506b7f1ffb8696
Dani Rashba <danirdd92@gmail.com>
2020-09-09 00:16:26 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      DisplayFizzBuzz(5, 2, 200);
+      Console.ReadKey();
+    }
+
+    static void DisplayFizzBuzz(int num = 3, int multiplayer = 5, int count = 100)
+    {
+      for (int i = 0; i < count; i++)
+        Console.WriteLine(GetOutputNumber(i, num, multiplayer));
+    }
+
+    static string GetOutputNumber(int i, int num, int multiplayer) =>
+     (i % num == 0, i % multiplayer == 0) switch
+     {
+       (true, true) => "FizzBuzz",
+       (true, _) => "Fizz",
+       (_, true) => "Buzz",
+       _ => i.ToString()
+     };
+
+  }
+}
